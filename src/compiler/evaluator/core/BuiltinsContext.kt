56eb3e53d51575d3ca0ed703c6028c7e4748c891
Rami Sabbagh <rami.sab07@gmail.com>
2023-01-08 17:41:09 +0300

    Add missing builtin constants in the context

--- src/compiler/evaluator/core/BuiltinsContext.kt
+++ src/compiler/evaluator/core/BuiltinsContext.kt
@@ -1,10 +1,19 @@
 package compiler.evaluator.core
 
+import compiler.evaluator.builtins.constants.PyNone
+import compiler.evaluator.builtins.constants.PyNotImplemented
 import compiler.evaluator.builtins.functions.PyAppend
 import compiler.evaluator.builtins.functions.PyPrint
 import compiler.evaluator.builtins.functions.PyRange
+import compiler.evaluator.builtins.types.PyBool
 
-val builtins = hashMapOf<String, PyObject>(
+val builtins = hashMapOf(
+    // Constants
+    "None" to PyNone,
+    "NotImplemented" to PyNotImplemented,
+    "True" to PyBool.of(true),
+    "False" to PyBool.of(false),
+    // Functions
     "print" to PyPrint,
     "range" to PyRange,
     "append" to PyAppend,
