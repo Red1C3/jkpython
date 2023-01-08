82898d3fabdfa2c85dfd164dc92b0753370f9bb2
Rami Sabbagh <rami.sab07@gmail.com>
2023-01-08 17:29:50 +0300

    `PyPrint` now uses `__str__`

--- src/compiler/evaluator/builtins/functions/PyPrint.kt
+++ src/compiler/evaluator/builtins/functions/PyPrint.kt
@@ -1,14 +1,20 @@
 package compiler.evaluator.builtins.functions
 
 import compiler.evaluator.builtins.constants.PyNone
+import compiler.evaluator.builtins.types.PyString
 import compiler.evaluator.core.PyCallable
 import compiler.evaluator.core.PyObject
 
 object PyPrint : PyCallable {
     // TODO: Add key based arguments
-    // TODO: Do string casting the python way
     override fun call(arguments: List<PyObject>): PyObject {
-        println(arguments.joinToString(" ") { it.toString() })
+        println(arguments.joinToString(" ") {
+            when (val value = it.__str__()) {
+                is PyString -> value.value
+                else -> error("TypeError: __str__ returned non-string (${value.__repr__().value})")
+            }
+        })
+
         return PyNone
     }
 }
